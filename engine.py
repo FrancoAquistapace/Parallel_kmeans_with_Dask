fa0f481f74029ee00da17433c8a0fabf02acd7b8
Franco Aquistapace <francoaquistapace@192.168.1.54>
2025-06-24 19:32:23 +0200

    Some progress with initial setup of the data, developed squared distance and cost functions for engine, and tested on small dataset.

--- engine.py
+++ engine.py
@@ -32,8 +32,23 @@ def get_first_sample(data_path):
     return pd.read_csv(data_path, nrows=1)
 
 
+def get_XXT_term(X):
+    '''
+    Params:
+        X : Dask array or dataframe
+            Array containing data points.
+    Output:
+        Returns the self multiplication term from the
+        squared distance matrix formula.
+    '''
+    # Turn into array
+    X_da = da.array(X)
+    # Get diagonal of X*X_T
+    XXT = da.diag(da.matmul(X_da, da.transpose(X_da)))
+    return XXT
 
-def cost_function(C, X):
+
+def partial_squared_dist_matrix(C, X):
     '''
     Params:
         C : Dask array or dataframe
@@ -41,8 +56,45 @@ def cost_function(C, X):
         X : Dask array or dataframe
             Array containing data points.
     Output:
+        Returns the partial squared distance matrix, 
+        evaluated over the set of points X with respect 
+        to the centroids C. The partial matrix is defined
+        as:
+         D'^2 = 2*XC^T + CC^T
+    '''
+    # Turn into arrays
+    X_da = da.array(X)
+    
+    # Calculate XC term
+    XC_term = da.matmul(X_da, da.transpose(C))
+
+    # Calculate CC term
+    CC_term = da.einsum('ij,ji -> i', C, da.transpose(C))
+    
+    return 2*XC_term + CC_term
+    
+
+def cost_function(C, X, XXT):
+    '''
+    Params:
+        C : Dask array or dataframe
+            Array containing centroid locations
+        X : Dask array or dataframe
+            Array containing data points.
+        XXT : Dask array or dataframe
+            Array with same number of rows as X, 
+            containing the X*X^T term of the
+            squared distance matrix.
+    Output:
         Returns the K-means cost function, evaluated
         over the set of points X with respect to the
         centroids C.
     '''
-    return 
+    # First, get partial squared distances
+    D2 = partial_squared_dist_matrix(C, X)
+
+    # Minimize over C axis and sum
+    D2_min_sum = da.sum(da.min(D2, axis=1))
+
+    # Add to XXT sum and return
+    return da.sum(XXT) + D2_min_sum
